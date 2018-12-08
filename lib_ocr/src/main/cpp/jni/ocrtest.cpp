6531b7dd1355f25c871d0c9003bfcd4b75a82a13
kalu <153437803@qq.com>
2018-12-08 11:44:34 +0800

    test

---
+++ lib_ocr/src/main/cpp/jni/ocrtest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kalu on 2018/12/8.
+//
+
+#include "ocrtest.h"
---
+++ lib_ocr/src/main/cpp/jni/ocrtest.h
@@ -0,0 +1,14 @@
+//
+// Created by kalu on 2018/12/8.
+//
+
+#ifndef CARDOCR_OCRTEST_H
+#define CARDOCR_OCRTEST_H
+
+
+class ocrtest {
+
+};
+
+
+#endif //CARDOCR_OCRTEST_H
