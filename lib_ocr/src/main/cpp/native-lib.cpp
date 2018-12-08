81fd19e307bea39d3bcda33a7d3b8aba134d4b12
kalu <153437803@qq.com>
2018-12-08 11:41:21 +0800

    test

--- lib_ocr/src/main/cpp/native-lib.cpp
+++ lib_ocr/src/main/cpp/native-lib.cpp
@@ -1,10 +1,10 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_kalu_myapplication_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_lib_kalu_ocr_OcrJni_stringFromJNI(JNIEnv *env, jclass type) {
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
