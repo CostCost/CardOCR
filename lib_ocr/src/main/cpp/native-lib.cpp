09b42b32fc6cc5e8cb120c92251b8eccffd39222
kalu <153437803@qq.com>
2018-12-25 17:49:09 +0800

    test

--- lib_ocr/src/main/cpp/native-lib.cpp
+++ lib_ocr/src/main/cpp/native-lib.cpp
@@ -9,17 +9,17 @@ Java_lib_kalu_ocr_OcrJni_stringFromJNI(JNIEnv *env, jclass type) {
     return env->NewStringUTF(hello.c_str());
 }
 
-//extern "C"
-//JNIEXPORT jobject JNICALL
-//Java_exocr_exocrengine_EXOCREngine_nativeRecoIDCardStillImage(JNIEnv *env, jclass type,
-//                                                              jobject bitmap, jint tryhard,
-//                                                              jint bwantimg, jbyteArray bresult_,
-//                                                              jint maxsize, jintArray rets_) {
-//    jbyte *bresult = env->GetByteArrayElements(bresult_, NULL);
-//    jint *rets = env->GetIntArrayElements(rets_, NULL);
-//
-//    // TODO
-//
-//    env->ReleaseByteArrayElements(bresult_, bresult, 0);
-//    env->ReleaseIntArrayElements(rets_, rets, 0);
-//}
+extern "C"
+JNIEXPORT jobject JNICALL
+Java_exocr_exocrengine_EXOCREngine_nativeRecoIDCardStillImage(JNIEnv *env, jclass type,
+                                                              jobject bitmap, jint tryhard,
+                                                              jint bwantimg, jbyteArray bresult_,
+                                                              jint maxsize, jintArray rets_) {
+    jbyte *bresult = env->GetByteArrayElements(bresult_, NULL);
+    jint *rets = env->GetIntArrayElements(rets_, NULL);
+
+    // TODO
+
+    env->ReleaseByteArrayElements(bresult_, bresult, 0);
+    env->ReleaseIntArrayElements(rets_, rets, 0);
+}
